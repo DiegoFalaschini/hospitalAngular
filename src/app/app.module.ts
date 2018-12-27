1f72ec320586cdc60a3e98482e4efc3f3aadc846
Diego Luis Falaschini <diegofalaschini@yahoo.com.ar>
2018-12-27 18:18:10 -0300

    Input, OutPut, ViewChild, Graficos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,29 @@ import { APP_ROUTES } from './app.routes';
 
 // Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { IncrementadorComponent } from './components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from './components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
      LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    
+
   ],
   imports: [
     BrowserModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,21 +5,31 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { ProgressComponent } from "./progress/progress.component";
 import { PAGES_ROUTES } from "./pages.routes";
+import { FormsModule } from "@angular/forms";
+import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
 
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+import { GraficoDonaComponent } from "../components/grafico-dona/grafico-dona.component";
 
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         Graficas1Component,
-        ProgressComponent,        
+        ProgressComponent, 
+        IncrementadorComponent,
+        GraficoDonaComponent  
     ],
     exports: [
         ProgressComponent
     ],
     imports: [
+        FormsModule,
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        ChartsModule,
+        
         
     ]
 })
